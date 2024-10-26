56e9fb8a692fe052d54affa546601b6512f5639d
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-10-26 17:30:07 +0530

    first commit

---
+++ source/main.js
@@ -0,0 +1,9 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
+
+import mobile_nav_func from './utils/mobile-nav'
+
+mobile_nav_func()
---
+++ source/utils/mobile-nav.js
@@ -0,0 +1,30 @@
+const mobile_nav_func = ()=>{
+const menubars = document.querySelector(".header__bars")
+const mobileNav = document.querySelector(".mobile-nav")
+const mobileLinks = document.querySelectorAll(".mobile-nav__link")
+// state
+let isMobileNavOpen = false
+
+menubars.addEventListener("click",()=>{
+
+    isMobileNavOpen = !isMobileNavOpen
+    if(isMobileNavOpen){
+        mobileNav.style.display = "flex"
+        document.body.style.overflowY = 'hidden'
+    }else{
+        mobileNav.style.display = "none"
+        document.body.style.overflowY = 'hidden'
+    }
+
+})
+
+mobileLinks.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        isMobileNavOpen = false
+        mobileNav.style.display = "none"
+        document.body.style.overflowY = "auto"
+    })
+})
+
+}
+export default mobile_nav_func;
