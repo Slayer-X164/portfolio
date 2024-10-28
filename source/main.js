4fa390735c220cd40cdb901695f18f21b89bb2af
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-10-28 19:45:16 +0530

    work section done

--- source/main.js
+++ source/main.js
@@ -4,6 +4,7 @@ import '../styles/components/header.css';
 import '../styles/components/mobile-nav.css';
 import '../styles/components/hero.css'
 import '../styles/components/about.css'
+import '../styles/components/work.css'
 import '../styles/utils.css';
 
 import mobile_nav_func from './utils/mobile-nav'
